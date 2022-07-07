38f651e63f405e59b611a29b76fc1145d3d9bd19
adnanioricce <adnanioricce@outlook.com>
2022-07-06 22:19:25 -0300

    adding utils to random generate values for sampling;maybe use a noise function

--- src/Utils.h
+++ src/Utils.h
@@ -7,6 +7,7 @@
 #include <glm/vec3.hpp>
 #include <glm/vec2.hpp>
 #include <sstream>
+#include <random>
 std::string vec3_str(glm::vec3 v){
     std::stringstream ss;
     ss << "(" << v.x << "," << v.y << "," << v.z << ")";
@@ -16,5 +17,19 @@ std::string vec2_str(glm::vec2 v){
     std::stringstream ss;
     ss << "(" << v.x << "," << v.y << ")";
     return ss.str();
+}
+inline float random_float(float min,float max){
+    static std::uniform_real_distribution<float> distribution(min,max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+inline float random_float(){
+    return random_float(0.0f,1.0f);
+}
+inline glm::vec3 random_vec3(){
+    return glm::vec3(random_float(),random_float(),random_float());
+}
+inline glm::vec3 random(float min, float max) {
+    return glm::vec3(random_float(min,max), random_float(min,max), random_float(min,max));
 }
 #endif //RAYTRACER_UTILS_H
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,42 @@
 #include "HittableObjects.h"
 #include "color.h"
 #include "Utils.h"
+struct Camera {
+    float aspect_ratio;
+    float viewport_height;
+    float viewport_width;
+    float focal_length;
+
+    glm::vec3 origin;
+    glm::vec3 horizontal;
+    glm::vec3 vertical;
+    glm::vec3 lower_left_corner;
+    Camera(){
+        this->aspect_ratio = 16.0 / 9.0;
+        this->viewport_height = 2.0;
+        this->viewport_width = aspect_ratio * viewport_height;
+        this->focal_length = 1.0;
+
+        this->origin = glm::vec3(0, 0, 0);
+        this->horizontal = glm::vec3(viewport_width, 0.0, 0.0);
+        this->vertical = glm::vec3(0.0, viewport_height, 0.0);
+        this->lower_left_corner = origin - horizontal/2.0f - vertical/2.0f - glm::vec3(0, 0, focal_length);
+    }
+    Camera(glm::vec3 origin){
+        this->aspect_ratio = 16.0 / 9.0;
+        this->viewport_height = 2.0;
+        this->viewport_width = aspect_ratio * viewport_height;
+        this->focal_length = 1.0;
+
+        this->origin = origin;
+        this->horizontal = glm::vec3(viewport_width, 0.0, 0.0);
+        this->vertical = glm::vec3(0.0, viewport_height, 0.0);
+        this->lower_left_corner = origin - horizontal/2.0f - vertical/2.0f - glm::vec3(0, 0, focal_length);
+    }
+};
+Ray get_ray(Camera cam,glm::vec2 uv){
+    return Ray(cam.origin,cam.lower_left_corner + uv.x * cam.horizontal + uv.y * cam.vertical - cam.origin);
+}
 //OBS: Origin here is different from the tutorial.
 //the origin in here is located at (width / 2,height / 2)
 const auto aspect_ratio = 16.0 / 9.0;
@@ -26,66 +62,73 @@ const int image_height = static_cast<int>(image_width / aspect_ratio);
 //has_hit_sphere = hit function of sphere class on tutorial
 // Structures Functions
 
-void setPixel(EasyBMP::Image& img,glm::vec2 pos,glm::vec3 color){
+void setPixel(EasyBMP::Image& img,glm::vec2 pos,glm::vec3 color,const int samples_per_pixel){
+    auto scale = 1.0f / samples_per_pixel;
+    color.r *= scale;
+    color.g *= scale;
+    color.b *= scale;
+    auto c = glm::clamp(color,glm::vec3(0.0),glm::vec3(255.9f));
     img.SetPixel(pos.x,pos.y,EasyBMP::RGBColor(color.x,color.y,color.z));
 }
+//TODO: Sections in RayTracingInOneWeekend to be implemented:
+    //TODO: Section 8 - Diffuse Materials
+        //TODO: 8.1 -> A Simple Diffuse Material
+            //TODO: Create a perlin noise like function to avoid using a random function for sampling
+        //TODO: 8.2 -> Limit the Number of Child Rays
+        //TODO: ...
 
+//TODO: Create a class to define scenes
+//TODO(OPTIONAL): Create a struct to define different object structures to be rendered
 int main(){
   // Image
 
     EasyBMP::RGBColor black(64,88,127);
     EasyBMP::Image img(image_width,image_height,"output.bmp",black);
     std::cout << "image width:" << image_width << ", height:" << image_height << std::endl;
+    const int samples_per_pixel = 2;
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
     auto origin = glm::vec3(image_width / 2,image_height / 2,0);
-    //auto origin = glm::vec3(0,0,0);
-    auto horizontal = glm::vec3(viewport_width,0,0);
-    auto vertical = glm::vec3(0,viewport_height,0);
-    auto lower_left_corner = origin -
-                             (horizontal / 2.0f)
-                             //glm::vec3(1,0,0)
-                             - (vertical / 2.0f) - glm::vec3(0,0, focal_length);
-  // World
-  std::vector<HittableSphere> spheres = {
-      HittableSphere(glm::vec3(origin.x,origin.y,-1),0.5f),
-      HittableSphere(glm::vec3(origin.x,origin.y + 100.0f,-1.0f),100.0f)
-  };
+    Camera cam(origin);
+    // World
+    std::vector<HittableSphere> spheres = {
+      HittableSphere(glm::vec3(cam.origin.x,cam.origin.y,-1),0.5f),
+      HittableSphere(glm::vec3(cam.origin.x,cam.origin.y + 100.0f,-1.0f),100.0f)
+    };
 
+    std::cout << "lower_left_corner: (" << cam.lower_left_corner.x << ","
+          << cam.lower_left_corner.y << ","
+          << cam.lower_left_corner.z << ")"
+          << std::endl;
+      // Render
+      //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::stringstream logCsv;
+    logCsv << "Position,Origin,Direction,Color" << std::endl;
 
-  std::cout << "lower_left_corner: (" << lower_left_corner.x << ","
-      << lower_left_corner.y << ","
-      << lower_left_corner.z << ")"
-      << std::endl;
-  // Render
-  //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-  std::stringstream logCsv;
-  logCsv << "Position,Origin,Direction,Color" << std::endl;
+    for(int j = image_height-1;j >= 0; --j){
+       for(int i = 0;i < image_width;++i){
+           glm::vec3 color(0,0,0);
+           glm::vec2 pos;
+           Ray r;
+           for(int s = 0;s < samples_per_pixel;++s) {
+               pos = glm::vec2(
+                               (float(i + random_float()) / (image_width - 1)),
+                               (float(j + random_float()) / (image_height - 1)));
+               r = get_ray(cam,pos);
+               //glm::vec3 color = ray_color(r);
+               //setPixel(img,pos,color);
+               color += ray_color(r, cam.origin, spheres);
 
-  for(int j = image_height-1;j >= 0; --j){
-      for(int i = 0;i < image_width;++i){
-          glm::vec2 pos = 
-                  //glm::vec2(i,j);
-                  glm::vec2(
-                  (float(i) / (image_width - 1)),
-                  (float(j) / (image_height - 1)));
-          Ray r(origin,lower_left_corner + (pos.x * horizontal) + (pos.y * vertical) - origin);
-          //glm::vec3 color = ray_color(r);
-          //setPixel(img,pos,color);
-          glm::vec3 pixel_color = ray_color(r,origin,spheres);
-          logCsv << "r Pos = " + vec2_str(pos) + ", Origin = " << vec3_str(r.origin) 
-              << ", Direction = " << vec3_str(r.direction)
-              << ", Color = " << vec3_str(pixel_color)
-              << std::endl;
-          setPixel(img,glm::vec2(i,j),pixel_color);
-      }
-  }
-  std::ofstream csvOut("output.csv");
-  csvOut << logCsv.str();
-  csvOut.close();
-  img.Write();
-  return 0;
+           }
+           logCsv << "r Pos = " + vec2_str(pos) + ", Origin = " << vec3_str(r.origin)
+                  << ", Direction = " << vec3_str(r.direction)
+                  << ", Color = " << vec3_str(color)
+                  << std::endl;
+           setPixel(img,glm::vec2(i,j),color,samples_per_pixel);
+       }
+    }
+   std::ofstream csvOut("output.csv");
+   csvOut << logCsv.str();
+   csvOut.close();
+   img.Write();
+   return 0;
 }
